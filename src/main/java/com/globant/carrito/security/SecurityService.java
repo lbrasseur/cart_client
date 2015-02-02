ef16adc68b4a29b6e86649fd900cf76994ddce63
Lautaro Brasseur <lautaro.brasseur@globant.com>
2015-02-02 18:55:04 -0300

    Integración con sesión HTTP

--- src/main/java/com/globant/carrito/security/SecurityService.java
+++ src/main/java/com/globant/carrito/security/SecurityService.java
@@ -1,5 +1,7 @@
 package com.globant.carrito.security;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,11 +12,28 @@ import com.globant.carrito.StatusDto;
 
 @RestController
 public class SecurityService {
+	private static final String USER_ID_KEY = "userId";
 
 	@RequestMapping(value = "/service/login", method=RequestMethod.POST)
 	@ResponseBody
-	public StatusDto login(@RequestBody LoginDto dto) {
-		return new StatusDto(dto.getUsername().equals("bart")
+	public StatusDto login(@RequestBody LoginDto dto, HttpServletRequest request) {
+		boolean isValid = (dto.getUsername().equals("bart")
 				&& dto.getPassword().equals("simpson"));
+
+		if (isValid) {
+			// Almacenando el username a modo de ejemplo.
+			// Probablemente es mejor almacenar el ID, pero para
+			// eso seria necesario hacer una consulta a la base.
+			request.getSession().setAttribute(USER_ID_KEY, dto.getUsername());
+		}
+
+		return new StatusDto(isValid);
+	}
+
+	@RequestMapping(value = "/service/logout", method=RequestMethod.GET)
+	@ResponseBody
+	public StatusDto logout(HttpServletRequest request) {
+		request.getSession().invalidate();
+		return new StatusDto(true);
 	}
 }
